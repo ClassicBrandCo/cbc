c890319b662f1c1defc6683d2e4454b4cf4ab8f2
Developer <developer@example.com>
2026-03-15 18:28:38 +0300

    v03

---
+++ sw.js
@@ -0,0 +1,24 @@
+// PWA Service Worker - Cache static assets
+const CACHE_NAME = 'cbc-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/contact.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js',
+  '/assets/img/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
